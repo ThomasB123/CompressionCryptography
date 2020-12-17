4fec1dbb42e4f32dc5390775c473994cae6471a3
ThomasB123 <thomas@thebutterfields.org>
2020-12-17 17:24:40 +0000

    c

--- decoder.py
+++ decoder.py
@@ -1,1 +1,51 @@
+
+import os
 import sys
+
+
+if len(sys.argv) == 1:
+    inFile = input('Enter a .lz file name > ')
+elif len(sys.argv) > 2:
+    print('You must only give 1 file name')
+    exit()
+else:
+    inFile = sys.argv[1]
+if '.' not in inFile:
+    print('Please give a valid file name')
+    exit()
+if inFile.split('.')[1] != 'lz':
+    print('You must give a .lz file')
+    exit()
+if not os.path.isfile(inFile):
+    print('That is not a valid file name')
+    exit()
+fileName = inFile.split('.')[0]
+decodedFile = fileName + '-decoded.tex'
+
+
+# Size of input file
+inputSize = os.path.getsize(inFile)
+print("Input file: \t" + inFile)
+print("Input size: \t" + str(inputSize) + "\n")
+
+########################################
+# do decoding here
+
+
+
+
+with open(decodedFile,'w') as fout:
+    fin = open(inFile,'r')
+    for line in fin:
+        fout.write(line)
+
+
+
+
+
+########################################
+
+# Runs your decoder and prints out size of decoded file
+decodedSize = os.path.getsize(decodedFile)
+print("Decoded file: \t" + decodedFile)
+print("Decoded size: \t"  + str(decodedSize) + "\n")
--- encoder.py
+++ encoder.py
@@ -1,1 +1,59 @@
+
+# up to 6 ideas explained in the report (5 marks each), format same as Max's lecture notes.
+# Can implement more than 6 ideas as long as different.
+# he is using just zip file, gives 30% file size increase, to check for not-trivial answers, would get 2 marks for that.
+# can't have all 6 ideas from lecture notes, because would be redundant
+# should have some original ideas, from looking elsewhere and thinking of other things yourself
+# file will be roughly 1 MB, can take 10 or 20 minutes if neccessary and he's fine with that
+# if program works on this script then it should work
+# code doesn't need to be commented, is good practice for yourself but he doesn't require it
+
+
+import os
 import sys
+
+if len(sys.argv) == 1:
+    inFile = input('Enter a .tex file name > ')
+elif len(sys.argv) > 2:
+    print('You must only give 1 file name')
+    exit()
+else:
+    inFile = sys.argv[1]
+if '.' not in inFile:
+    print('Please give a valid file name')
+    exit()
+if inFile.split('.')[1] != 'tex':
+    print('You must give a .tex file')
+    exit()
+if not os.path.isfile(inFile):
+    print('That is not a valid file name')
+    exit()
+fileName = inFile.split('.')[0]
+encodedFile = fileName + '.lz'
+
+
+# Size of input file
+inputSize = os.path.getsize(inFile)
+print("Input file: \t" + inFile)
+print("Input size: \t" + str(inputSize) + "\n")
+
+########################################
+# do encoding here
+
+
+
+with open(encodedFile,'w') as fout:
+    fin = open(inFile,'r')
+    for line in fin:
+        fout.write(line)
+
+
+
+
+########################################
+
+# Runs your encoder and prints out size of encoded file
+encodedSize = os.path.getsize(encodedFile)
+print("Encoded file: \t" + encodedFile)
+print("Encoded size: \t" + str(encodedSize) + "\n")
+
