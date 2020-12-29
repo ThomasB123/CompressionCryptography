64d7cd9bc72ff43febb1064e551a0897ce4bbef1
ThomasB123 <44202676+ThomasB123@users.noreply.github.com>
2020-12-29 01:52:51 +0000

    updated first half tester

--- firstHalf.py
+++ firstHalf.py
@@ -1,13 +1,14 @@
 
 import os
 import json
+import subprocess
 
 with open('data2.json','r') as infile:
     data = json.load(infile)
 print(len(data))
 
 for x in data:
-    encrypted = subprocess.run(['encrypt.exe',data],stdout=subprocess. PIPE).stdout.decode('utf-8')[:-2]
+    encrypted = subprocess.run(['encrypt.exe',x],stdout=subprocess.PIPE).stdout.decode('utf-8')[:-2]
     if encrypted == '903408ec4d951acf':
         print(x)
         print(encrypted)
